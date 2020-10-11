ac606e886b52febc85d65dbd702c92d16b2d2c37
Olle Strand <ollestrand02@gmail.com>
2020-10-11 15:07:40 +0200

    Create HistoricalDataService.cs

---
+++ IgBotTraderCLI/Services/HistoricalDataService.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TANet.Contracts.Models;
+using TANet.Core;
+
+namespace IgBotTraderCLI.Services
+{
+    public class HistoricalDataService
+    {
+        private RestClient Client { get; set; }
+        public Dictionary<DateTime, List<Candle>> HistoricalData { get; set; }
+
+        public HistoricalDataService()
+        {
+            Client = new RestClient("https://yahoo-finance15.p.rapidapi.com/api/yahoo");
+        }
+
+
+        public void LoadHistoricalData()
+        {
+
+        }
+
+    }
+}
